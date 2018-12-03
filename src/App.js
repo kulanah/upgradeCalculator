cb1e2693cda22b969bf190569b0c1057d125d673
Devon <devonthebutler@gmail.com>
2018-12-03 12:23:16 -0800

    added user stats to main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserStats from './components/UserStats';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <UserStats />
 
       </div>
     );
