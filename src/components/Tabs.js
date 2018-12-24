9d3ad9a211ae1849c9c6765f40f2f81a4432c33f
Devon <devonthebutler@gmail.com>
2018-12-23 16:26:42 -0800

    reworked tabs component to use generic tab component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
-import WelcomeTab from './WelcomeTab';
-import UserTab from './UserTab';
+import Tab from 'components/Tab';
+
+import WelcomeContent from 'components/tabbedContent/WelcomeContent'
+import UserContent from 'components/tabbedContent/UserContent'
+import FillerContent from 'components/tabbedContent/FillerContent'
 
 class Tabs extends Component {
   constructor(props) {
     super(props);
     
     this.selectedTab = props.selected || 0;
-    this.tabTitles = ['Welcome', 'User Stats']
-    this.tabs = [<WelcomeTab />, <UserTab />]
-  }
+    this.tabTitles = ['Welcome', 'User Stats', 'Gloves', 'Shoes'];
+    this.tabs = [
+      <Tab text='Welcome' content={<WelcomeContent />} index='0'/>,
+      <Tab text='User Stats' content={<UserContent/>} index='1'/>,
+      <Tab text='Shoes' content={<FillerContent/>} index='2'/>,
+      <Tab text='Gloves' content={<FillerContent/>} index='3'/>,
+    ];
+  };
 
   render() {
     return (
       <div className='TabsWindow'>
-        {this.tabTitles.map(tab => {
-          return <span className='Tab'>{tab}</span>
+        {this.tabs.map(tab => {
+          return tab.props.text;
         })}
-        
+
+        <div className="TabbedContent">
+          {this.tabs[this.selectedTab]}
+        </div>
       </div>
     );
   }
