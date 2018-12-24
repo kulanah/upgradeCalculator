313ad67fb34f0ccc4219b5ba4831e561da493f96
Devon <devonthebutler@gmail.com>
2018-12-23 17:15:47 -0800

    added onclicks for tab selection tot he tab component

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 class Tab extends Component {
   constructor(props) {
     super(props);
-    
+
+    this.selectTab = this.selectTab.bind(this);
+  }
+
+  selectTab(){
+    this.props.tabClick(this.props.index);
   }
   
   render() {
     return (
-      <span className='Tab'>
-        {this.props.content}
+      <span className='Tab' index={this.props.index} onClick={this.selectTab}>
+        {this.props.text}
         
       </span>
     );
