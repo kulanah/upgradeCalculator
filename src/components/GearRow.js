b3a5972317ae299e469d7f616a2efa5315fd6c98
kulanah <devonthebutler@gmail.com>
2019-02-10 14:05:52 -0800

    mapped gear rows props to its stte

--- src/components/GearRow.js
+++ src/components/GearRow.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class GearRow extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { ...this.props.stats};
+  }
+  
   render() {
     return (
       <div className='GearRow'>
